2c7b5378ca94a14adcd6110792c9299808632f73
jimtom2713 <jwilliamson528@gmail.com>
2015-07-03 08:49:41 -0700

    updates

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,8 +2,15 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  list.idx = 0;
+  list.storage = {};
 
   list.addToTail = function(value){
+    var newNode = new Node(value);
+    if(list.head === null){
+      list.head = newNode;
+    }
+    list.tail = newNode
   };
 
   list.removeHead = function(){
