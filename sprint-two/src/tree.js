d15fceaf6b839ffd890c0e0578edadefa2c4bf2d
jimtom2713 <jwilliamson528@gmail.com>
2015-07-03 11:34:30 -0700

    'passed test for tree'

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,23 +3,32 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
-
-
-
-
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+	this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target){
-
+	var foundTarget = false;
+	var checkChildren = function(node){
+		if (node.value === target) { 
+			foundTarget = true; 
+			return;
+		} else {
+			for (var i=0; i<node.children.length; i++) {
+				checkChildren(node.children[i]);
+			}
+		}
+	};
+	checkChildren(this);
+	return foundTarget;
 };
 
 
